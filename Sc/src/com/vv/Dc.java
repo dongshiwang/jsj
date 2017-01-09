c000a1b96439d603d43e51762d79e04e0d83dd69
Administrator <Administrator@8WM57CEVWPKK9LE>
2017-01-09 14:30:44 +0800

    阿萨德

--- Sc/src/com/vv/Dc.java
+++ Sc/src/com/vv/Dc.java
@@ -1,10 +1,11 @@
 package com.vv;
 
 public class Dc {
- public static void main(String[] args) {
-	System.err.println(11);
-}
-	public String ss(){
+	public static void main(String[] args) {
+		System.err.println(11);
+	}
+
+	public String ss() {
 		return "ss";
 	}
 }
